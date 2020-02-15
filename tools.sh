bf836d62a1ddb2cbee1cf1f36853b63abcb8aa2d
Morten Omholt-Jensen <morten@omholt-jensen.com>
2020-02-15 21:26:24 +0100

    updated setup script

--- tools.sh
+++ tools.sh
@@ -19,20 +19,17 @@ brew cask install caprine
 brew cask install spotify
 brew cask install 1password
 brew cask install alfred
-brew cask install skype-for-business
 
 # Browsers
 brew cask install google-chrome
-brew cask install google-chrome-canary
 brew cask install firefox
 
-# Terminal 
+# Terminal
 brew cask install iterm2
 
 # Design tools
 brew cask install figma
 brew cask install zeplin
-brew cask install homebrew/cask-versions/google-chrome-canary
 
 # Development
 brew cask install docker
@@ -40,17 +37,12 @@ brew cask install minikube
 brew cask install tableplus
 brew install neovim
 brew cask install visual-studio-code
-brew cask install geektool
-brew cask install cyberduck
 brew cask install java
-brew cask install virtualbox
-brew install grpcurl
 
 
 # -------------------------------
 # --------- Dev tools ----------
 # -------------------------------
-
 brew install kubectl
 brew install kubectx
 brew install jq
@@ -58,11 +50,9 @@ brew install vault
 brew install consul
 brew install cmake
 brew install hub
-brew install kubeless
 
 # Python
 brew install python3
-sudo easy_install pip
 
 # databases
 brew install postgres
@@ -80,7 +70,6 @@ gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703
 brew install go
 brew install dep
 
-
 # Node
 brew install node
 npm install -g react-native-cli
@@ -94,32 +83,11 @@ npm install -g expo-cli
 # -------------------------------
 brew install fzf
 $(brew --prefix)/opt/fzf/install --all
-brew install circleci
-circleci setup
-circleci config validate
 brew install ack
-brew install httpie
 brew install z
 brew install tmux
 brew install zsh-autosuggestions
 brew install watch
 
-pip3 install awscli
-pip3 install aws-google-auth
-
-if [ ! -x "$(command -v aws-iam-authenticator)" ]; then
-    echo "Installing AWS IAM authenticator for macos"
-    curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/darwin/amd64/aws-iam-authenticator
-    curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/darwin/amd64/aws-iam-authenticator.sha256
-    openssl sha1 -sha256 aws-iam-authenticator
-    chmod +x ./aws-iam-authenticator
-    mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
-    echo "Cleaning up IAM install"
-    rm aws-iam-authenticator.sha256
-    rm aws-iam-authenticator
-fi
-
-
 # Install snazzy
 echo "Recommended install: Snazzy (https://github.com/sindresorhus/iterm2-snazzy)"
-
