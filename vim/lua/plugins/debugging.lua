a23b2abe779af58577a7e697be66771ee087c8ff
Morten Omholt-Jensen <morten@omholt-jensen.com>
2025-04-22 14:54:16 +0200

    vim debugging for go

--- vim/lua/plugins/debugging.lua
+++ vim/lua/plugins/debugging.lua
@@ -3,16 +3,14 @@ return {
 		"mfussenegger/nvim-dap",
 		config = function()
 			local dap = require("dap")
-			dap.adapters.go = { type = "server", host = "127.0.0.1", port = 38697 }
-			dap.configurations.go = { { type = "go", name = "Debug", request = "launch", program = "${file}" } }
 
 			local keymap = vim.keymap.set
 
-			keymap("n", "<F5>", require("dap").continue, { desc = "Start/Continue Debugging" })
-			keymap("n", "<F10>", require("dap").step_over, { desc = "Step Over" })
-			keymap("n", "<F11>", require("dap").step_into, { desc = "Step Into" })
-			keymap("n", "<F12>", require("dap").step_out, { desc = "Step Out" })
-			keymap("n", "<leader>b", require("dap").toggle_breakpoint, { desc = "Toggle Breakpoint" })
+			keymap("n", "<leader>bb", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+			keymap("n", "<leader>bc", dap.continue, { desc = "Start/Continue Debugging" })
+			keymap("n", "<leader>bo", dap.step_out, { desc = "Step Out" })
+			keymap("n", "<leader>bn", dap.step_over, { desc = "Step Over" })
+			keymap("n", "<leader>bi", dap.step_into, { desc = "Step Into" })
 		end,
 	},
 
@@ -25,16 +23,25 @@ return {
 		config = function()
 			require("dapui").setup()
 
-			local dap = require("dap")
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				require("dapui").open()
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
 			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				require("dapui").close()
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
 			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				require("dapui").close()
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
 			end
 		end,
+	},
+
+	{
+		"leoluz/nvim-dap-go",
+		dependencies = { "mfussenegger/nvim-dap" },
+		opts = {},
 	},
 }
